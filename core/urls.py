796087e72dbea203e15589b5db820924c4e79979
Doru Muresanu <doru.muresanu@yahoo.ie>
2024-12-16 08:53:26 +0000

    Create form for adding MenuItem via frontend

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('menu/', views.menu, name='menu'),
+    path('menu/add/', views.add_menu_item, name='add_menu_item'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MenuItemForm
 from .models import MenuItem
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def add_menu_item(request):
+    if request.method == 'POST':
+        form = MenuItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('menu')  # Redirect to the menu page after successful form submission
+    else:
+        form = MenuItemForm()
+    return render(request, 'add_menu_item.html', {'form': form})
 
 def menu(request):
     menu_items = MenuItem.objects.all()
