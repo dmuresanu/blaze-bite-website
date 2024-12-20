a7d9649a5a47b78f95b2ae0441222fc8cc87a628
Doru Muresanu <doru.muresanu@yahoo.ie>
2024-12-20 22:12:23 +0000

    Connect Heroku, Connect PostgrSQL

--- blaze_bite/settings.py
+++ blaze_bite/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--$46wr$xx))hi(fye&%_brg%#3=#jp)f)(11$ww@b-28bglvca'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-dmuresanu-blazebitewebs-i7ks7zxaeks.ws-eu117.gitpod.io']
 
@@ -81,11 +85,15 @@ WSGI_APPLICATION = 'blaze_bite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:GeJ3ma1iOIor@ep-nameless-forest-a2jzavi0.eu-central-1.aws.neon.tech/aids_niece_evade_946733")
