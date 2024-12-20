2cdeebe9bf52eea80cbfd24ad4484eccb1949a6a
Doru Muresanu <doru.muresanu@yahoo.ie>
2024-12-20 22:54:58 +0000

    Connect Cloudinary

--- blaze_bite/settings.py
+++ blaze_bite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-dmuresanu-blazebitewebs-i7ks7zxaeks.ws-eu117.gitpod.io', '.herokuapp.com']
 
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'core',
 ]
 
--- env.py
+++ env.py
@@ -4,3 +4,7 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:GeJ3ma1iOIor@ep-nameless-forest-a2jzavi0.eu-central-1.aws.neon.tech/aids_niece_evade_946733")
 
 os.environ.setdefault("SECRET_KEY", "SECRET_KEY_BLAZE_BITE")
+
+os.environ.setdefault(
+    "CLOUDINARY_URL","cloudinary://<your_api_key>:<your_api_secret>@dxf7oesj6"
+)
