99e3f3d48447b86d78f98215353c2860c435316c
Doru Muresanu <doru.muresanu@yahoo.ie>
2024-12-21 01:50:28 +0000

    Create form for table booking with necessary fields

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,14 @@
 # core/forms.py
 from django import forms
 from .models import MenuItem
+from .models import Booking
 
 class MenuItemForm(forms.ModelForm):
     class Meta:
         model = MenuItem
         fields = ['name', 'description', 'price', 'image']
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['full_name', 'email', 'phone_number', 'number_of_people', 'date', 'time', 'special_requests']
