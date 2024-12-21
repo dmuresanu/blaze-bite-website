2621b15e83d58ef3ea11153ad94fe0ed7421647b
Doru Muresanu <doru.muresanu@yahoo.ie>
2024-12-21 04:23:43 +0000

    Add form validation for booking and update view to handle errors

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,7 @@
 # core/forms.py
 from django import forms
-from .models import MenuItem
-from .models import Booking
+from .models import MenuItem, Booking
+import datetime
 
 class MenuItemForm(forms.ModelForm):
     class Meta:
@@ -12,3 +12,15 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['full_name', 'email', 'phone_number', 'number_of_people', 'date', 'time', 'special_requests']
+    
+    def clean_date(self):
+        date = self.cleaned_data.get('date')
+        if date < datetime.date.today():
+            raise forms.ValidationError('The reservation date cannot be in the past.')
+        return date
+    
+    def clean_time(self):
+        time = self.cleaned_data.get('time')
+        if time < datetime.datetime.now().time():
+            raise forms.ValidationError('The reservation time cannot be in the past.')
+        return time
