dc76fbbaa3a6294bbd93bfb02ef7e0808649f1e8
Doru Muresanu <doru.muresanu@yahoo.ie>
2024-12-22 13:40:26 +0000

    Add URL routes for staff profile and profile editing pages

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('booking/success/', views.booking_success, name='booking_success'),
     path('menu/', views.menu, name='menu'),
     path('menu/add/', views.add_menu_item, name='add_menu_item'),
+    path('menu/edit/<int:item_id>/', views.add_menu_item, name='edit_menu_item'),  # Edit existing menu item
+    path('menu/delete/<int:item_id>/', views.delete_menu_item, name='delete_menu_item'),  # Delete menu item
+    path('staff/profile/', views.staff_profile, name='staff_profile'),  # Staff profile page
+    path('staff/edit-profile/', views.edit_profile, name='edit_profile'),  # Edit staff profile
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
 ]
